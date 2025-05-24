1d6e237ee901b845c5188eec0a38ad79b4f9731a
DiarBerisha <154000780+DiarBerisha@users.noreply.github.com>
2025-05-24 15:31:58 +0200

    pasword-reset

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ include_once "header.php";?>
         <p>Nese nuk kini nje llogari ne <a href="index.php">Rent A Car Diari</a> <a href="register.php">regjistrohuni</a>tani!</p>
         <input type="email" class="email-l" placeholder="Email" >
         <input type="password" class="password-l" placeholder="Fjalkalimi" >
-        <p><a href="forgot-password.php">Kam harruar fjalkalimin!</a></p>
+        <p><a href="forgot-password.html">Kam harruar fjalkalimin!</a></p>
         <button class="button-l">Kyçu</button>
     </form>
 </body>
